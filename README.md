90e6b39788101efddac7673c02ce9c8552e95fbc
Mike Slone <michael.slone@hulu.com>
2017-05-30 11:46:42 -0700

    SparkyTrails use LinkedList instead of a circular array, allowing much more logical computation. Fixed bug where disabling and re-enabling a trail caused a visual artifact. Moved spline into SparkyKit to remove SloneUtil dependency. Updated documentation.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # SparkyKit
 System for creating sparks, trails, and fireworks in the Unity game engine.
+
+## Classes
+
+### SparkyEmitter
+Component that creates sparks.  Will destroy itself after the parent is destroyed, but sparks will fade away gracefully.
+
+### SparkyTrail
+A trail that follows behind an object.  Will automatically destroy itself after the parent is destroyed and will fade the trail gracefully.
+
+## Other Classes
+
+### SparkySpark
+Sparks from a SparkyEmitter.
+
+### Spline
+Simple spline implementation for smooth curve evaluation.
--- Scripts/SparkyTrail.cs
+++ Scripts/SparkyTrail.cs
@@ -4,18 +4,30 @@ using System.Collections.Generic;
 
 public class SparkyTrail : MonoBehaviour {
 
-	private Vector3[] trailPoints;
+	private LinkedList<Vector3> pointList = new LinkedList<Vector3> ();
 	private LineRenderer lr;
-	private int frontPoint = 0;
 	private GameObject parentObj = null;
 	private float lastEmissionTime = 0.0f;
 	private bool fadingOut = false;
 
 	public SparkyInfo sparkInfo;
-	public int numPoints = 10;
+
+	[Tooltip("Time that passes between points being laid.")]
 	public float timeBetweenEmissions = 0.05f;
+
+	[Tooltip("Time it takes to fade out once the parent is destroyed.")]
 	public float fadeTime = 1.0f;
+	
+	[Tooltip("Number of total points laid by the trail. 2 = straight line")]
+	[Range(2,32)]
+	public int numPoints = 2;
+
+	[Tooltip("Number of spline points per point laid by the trail.")]
+	[Range(1,16)]
+	public int splinePrecision = 1;
+
 	public GameObject endpointObj = null;
+	public float idleSpeed = 0f;
 
 	// Use this for initialization
 	void Start () {
@@ -28,11 +40,7 @@ public class SparkyTrail : MonoBehaviour {
 		transform.SetParent (null);
 
 		lastEmissionTime = Time.time;
-		trailPoints = new Vector3[numPoints];
-		for (int i = 0; i < trailPoints.Length; i++) {
-			trailPoints [i] = transform.position;
-		}
-		numPoints = 2;
+		pointList.AddFirst (transform.position);
 
 		lr = SparkySpark.InitLineRenderer (gameObject, sparkInfo);
 		lr.transform.SetParent (transform);
@@ -40,17 +48,13 @@ public class SparkyTrail : MonoBehaviour {
 		SetPoints ();
 	}
 
-	IEnumerator Main()
+	void OnEnable()
 	{
-		yield return new WaitForSeconds (timeBetweenEmissions);
-
-		while (true) {
-			AddPoint ();
-
-			while (lastEmissionTime + timeBetweenEmissions > Time.time) {
-				SetPoints ();
-				yield return new WaitForFixedUpdate ();
-			}
+		// If we're re-enabling it, get rid of the old points and reposition at the parent object.
+		if (parentObj != null) {
+			transform.position = parentObj.transform.position;
+			pointList.Clear ();
+			pointList.AddFirst (transform.position);
 		}
 	}
 
@@ -58,89 +62,110 @@ public class SparkyTrail : MonoBehaviour {
 	{
 		float opacity = 1.0f;
 
-		float[] alphas = new float[lr.colorGradient.alphaKeys.Length];
-		for (int i = 0; i > lr.colorGradient.alphaKeys.Length; i++) {
-			alphas [i] = lr.colorGradient.alphaKeys [i].alpha;
-		}
+		float startAlpha = lr.startColor.a;
+		float endAlpha = lr.endColor.a;
 
 		while (opacity > 0) {
-			yield return new WaitForFixedUpdate ();
-			opacity -= (1.0f / fadeTime) * Time.deltaTime;
+			yield return new WaitForEndOfFrame ();
 
-			for (int i = 0; i > lr.colorGradient.alphaKeys.Length; i++) {
-				lr.colorGradient.alphaKeys [i].alpha = alphas [i] * opacity;
+			for (int i = 0; i < lr.colorGradient.alphaKeys.Length; i++) {
+				lr.startColor = new Color(lr.startColor.r, lr.startColor.g, lr.startColor.b, opacity * startAlpha);
+				lr.endColor = new Color(lr.endColor.r, lr.endColor.g, lr.endColor.b, opacity * endAlpha);
 			}
+
+			opacity -= (1.0f / fadeTime) * Time.deltaTime;
 		}
+
 		Destroy (gameObject);
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+	void FixedUpdate() {
+
+		bool addPoint = true;
+		// Fill the list before timing them out.
+		if (pointList.Count < numPoints) {
+			float timePassed = Time.time - lastEmissionTime;
+			if (timePassed < timeBetweenEmissions) {
+				addPoint = false;
+			}
+		}
+
 		if (parentObj != null) {
 			transform.position = parentObj.transform.position;
-		} else if (!fadingOut) {
+		}
+
+		SetPoints ();
+
+		// Add the point after because setpoints will use the current position anyway.
+		if (addPoint) {
+			AddPoint ();
+			lastEmissionTime = Time.time;
+		}
+
+		if (parentObj == null && !fadingOut) {
 			fadingOut = true;
 			StartCoroutine (FadeOutAndDie ());
 		}
 	}
 
-	void LateUpdate() {
-		if (parentObj != null) {
-			transform.position = parentObj.transform.position;
-			// Update the front point.
-			lr.SetPosition (numPoints, transform.position);
-		}
-	}
-
 	void AddPoint()
 	{
-		lastEmissionTime = Time.time;
-		frontPoint = ((frontPoint - 1) + trailPoints.Length) % trailPoints.Length;
-		trailPoints [frontPoint] = transform.position;
-		if (numPoints < trailPoints.Length) {
-			numPoints++;
+		pointList.AddLast (transform.position);
+
+		if (pointList.Count > numPoints) {
+			pointList.RemoveFirst ();
 		}
 	}
 
 	void SetPoints()
 	{
-		float pct = Mathf.Clamp01((Time.time - lastEmissionTime) / timeBetweenEmissions);
-		Vector3[] pList = new Vector3[numPoints + 1];
-		pList [numPoints] = transform.position;
-		Vector3 prevPos = transform.position;
-
-		for (int i = 0; i < numPoints; i++) {
-			int srcIndex = (frontPoint + i) % trailPoints.Length;
-			int dstIndex = numPoints - i - 1;
-
-			if (numPoints < trailPoints.Length) {
-				pList [dstIndex] = trailPoints [srcIndex];
-			} else {
-				pList [dstIndex] = Vector3.Lerp (trailPoints [srcIndex], prevPos, pct);
-			}
-
-			prevPos = trailPoints [srcIndex];
-		}
-
-		lr.positionCount = numPoints + 1;
-		lr.SetPositions (pList);
-	}
-
-	void UpdateFirstAndLast()
-	{
-		if (numPoints < 1) {
+		if (pointList.Count == 0) {
+			Debug.LogError ("pointList should never have zero points!");
 			return;
 		}
 
-		// Update the front point.
-		lr.SetPosition (numPoints, transform.position);
+		if (pointList.Count == 1) {
+			lr.positionCount = 2;
+			lr.SetPosition (0, pointList.First.Value);
+			lr.SetPosition (1, transform.position);
+		}
 
-		// Update the back point.
-		float pct = Mathf.Clamp01((Time.time - lastEmissionTime) / timeBetweenEmissions);
-		int lastIndex = (frontPoint + numPoints - 1) % trailPoints.Length;
-		int secondToLastIndex = (lastIndex - 1 + trailPoints.Length) % trailPoints.Length;
-		Vector3 lerped = Vector3.Lerp (trailPoints [lastIndex], trailPoints [secondToLastIndex], pct);
-		lr.SetPosition (0, lerped);
-		endpointObj.transform.position = lerped;
+		lr.enabled = pointList.Count > 1;
+		if (!lr.enabled) {
+			return;
+		}
+
+		Vector3[] pList = new Vector3[pointList.Count + 1];
+		pList [pointList.Count] = transform.position;
+		Vector3 prevPos = transform.position;
+
+		LinkedListNode<Vector3> lln = pointList.First;
+		int index = 0;
+		do {
+			if (parentObj != null) {
+				lln.Value += parentObj.transform.forward.normalized * idleSpeed;
+			}
+
+			pList [index] = lln.Value;
+			prevPos = lln.Value;
+			index++;
+			lln = lln.Next;
+		} while (lln != null);
+
+		if (splinePrecision <= 1) {
+			lr.positionCount = pList.Length;
+			lr.SetPositions (pList);
+		} else {
+			Slonersoft.Spline sp = new Slonersoft.Spline (pList);
+			sp.tension = 0.4f;
+
+			int totalPoints = (splinePrecision * pList.Length) + 1;
+			lr.positionCount = totalPoints;
+			for (int i = 0; i < totalPoints; i++) {
+				float t = (float)i / (float)(totalPoints - 1);
+				Vector3 pos = sp.Evaluate (t);
+				lr.SetPosition (i, pos);
+			}
+		}
 	}
 }
